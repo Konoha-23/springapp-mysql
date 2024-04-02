da6fe13ecfb497e0ebdfc3dd3eaf6618ff882495
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-04-01 18:07:29 -0700

    Update UserRepository.java

--- src/main/java/practice24/dragonballspring/UserRepository.java
+++ src/main/java/practice24/dragonballspring/UserRepository.java
@@ -2,7 +2,7 @@ package practice24.dragonballspring;
 
 import org.springframework.data.repository.CrudRepository;
 
-import practice24.dragonballspring.user;
+import practice24.dragonballspring.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
