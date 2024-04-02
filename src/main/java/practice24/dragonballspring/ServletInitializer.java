74f3223543977d89e64e0f5fc7f995a997a249e4
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-04-01 18:21:32 -0700

    Update ServletInitializer.java

--- src/main/java/practice24/dragonballspring/ServletInitializer.java
+++ src/main/java/practice24/dragonballspring/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(dragonballspringapplication.class);
+		return application.sources(DragonballSpringApplication.class);
 	}
 
 }
