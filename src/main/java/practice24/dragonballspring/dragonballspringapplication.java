b7c4fbfbb02d28d4dda48c9dfda0d0eaaac0dc74
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-04-01 18:20:39 -0700

    Update dragonballspringapplication.java

--- src/main/java/practice24/dragonballspring/dragonballspringapplication.java
+++ src/main/java/practice24/dragonballspring/dragonballspringapplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class dragonballspringapplication {
+public class DragonballSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(dragonballspringapplication.class, args);
+		SpringApplication.run(DragonballSpringApplication.class, args);
 	}
 
 }
