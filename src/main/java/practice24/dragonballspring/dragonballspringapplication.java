b9d1809e7d3a72c6d4ac6ba2cf6711bab704b972
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-04-01 18:06:33 -0700

    Update and rename DragonballSpringApplication.java to dragonballspringapplication.java

--- src/main/java/practice24/dragonballspring/dragonballspringapplication.java
+++ src/main/java/practice24/dragonballspring/dragonballspringapplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DragonballSpringApplication {
+public class dragonballspringapplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DragonballSpringApplication.class, args);
+		SpringApplication.run(dragonballspringapplication.class, args);
 	}
 
 }
