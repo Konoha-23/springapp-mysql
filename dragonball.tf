39d317006b67a21b0d3a49ad5e277e3fb8135f95
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-04-04 07:04:20 -0700

    updated repository link

--- dragonball.tf
+++ dragonball.tf
@@ -2,7 +2,7 @@ resource "helm_release" "dragonball-mysql" {
   name       = "dragonball-mysql"
   #create_namespace = true
   namespace  = "db" #konoha
-  repository = "https://konoha-23.github.io/springapp-mysql/"
+  repository = "https://konoha-23.github.io/springapp-mysql/" #updated repository link
   chart      = "springapp-mysql"
   values = [
     "${file("dragonball-mysql.yml")}"
