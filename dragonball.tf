bb1ce0a5a3a3daf187313cf723c486e754352850
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-04-04 05:38:10 -0700

    Update dragonball.tf

--- dragonball.tf
+++ dragonball.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "dragonball-mysql" {
   name       = "dragonball-mysql"
   #create_namespace = true
-  namespace  = "konoha" #konoha
+  namespace  = "dragonball" #konoha
   repository = "https://konoha-23.github.io/springapp-mongo/"
   chart      = "springapp-mysql"
   values = [
